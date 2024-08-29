30765a22c578f4396d7446f3b9044b6e91a44b04
Reeta Panthee <reetapanthee01@gmail.com>
2024-08-29 11:12:15 +0545

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        domine: ["Domine", "serif"],
+      },
+      animation: {
+        rotating: "rotating 5s linear infinite",
+      },
+      keyframes: {
+        rotating: {
+          "0%": { transform: "rotate(0.0deg)" },
+          "25%": { transform: "rotate(90deg)" },
+          "50%": { transform: "rotate(180deg)" },
+          "75%": { transform: "rotate(deg)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
